8ad6294bffb5868588879ace0f79fb8e5728d267
Faruk D <f.diblen@esciencecenter.nl>
2020-04-07 19:23:00 +0200

    check system executables and run anaconda installer

--- conda.js
+++ conda.js
@@ -1,19 +1,42 @@
 var shell = require('shelljs');
 
+shell.set('-e');
+
 module.exports = {
-    check_dependencies: function () {
-        if (!shell.which('curl')) {
-            shell.echo('Sorry, this script requires curl');
-            // shell.exit(1);
+
+    check_exe: function (exe_name) {
+        console.log('\n----------------------');
+        if (!shell.which(exe_name)) {
+            shell.echo('Sorry, this script requires ', exe_name);
+            shell.exit(1);
         } else {
-            shell.echo('Found curl');
-            var curl_version = shell.exec('curl --version', {silent:true}).stdout;
-            console.log(curl_version)
+            shell.echo('Found ', exe_name);
+            var exe_version = shell.exec(exe_name + ' --version', {silent:true}).stdout;
+            console.log(exe_version)
         }
+    },
+
+    check_dependencies: function () {
+        var vm = this;
+        _dependencies = ['curl', 'bash', 'wget']
+        _dependencies.forEach(function (_dep) {
+            vm.check_exe(_dep);
+        });
+    },
+
+    get_miniconda: function () {
+        const _mc_repo = 'https://repo.continuum.io/miniconda/'
+        const _mc_script = 'Miniconda3-latest-Linux-x86_64.sh'
+        const _get_opts = ' -O ./miniconda.sh'
 
         // Run external tool synchronously
-        if (shell.exec('ls -asl').code !== 0) {
-            shell.echo('Error: ls command failed');
+        if (shell.exec('wget ' + _mc_repo + _mc_script + _get_opts).code !== 0) {
+            shell.echo('Error: download failed');
+            shell.exit(1);
+        }
+
+        if (shell.exec('bash ~/miniconda.sh -b -p ~/miniconda && rm -f ~/miniconda.sh').code !== 0) {
+            shell.echo('Error: conda installation failed');
             shell.exit(1);
         }
     }
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ try {
 
     // conda functions
     conda.check_dependencies();
+    conda.get_miniconda();
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
