e4c1f771be9d2074fc3cffa6ee624ac877d07bee
Faruk D <f.diblen@esciencecenter.nl>
2020-04-07 03:26:10 +0200

    add simple functions to test

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+import * as conda from "./conda";
 
 try {
-    // `env-file-name` input defined in action metadata file
+    // inputs
     const nameToGreet = core.getInput('env-file-name');
     console.log(`Environment file: ${env-file-name}!`);
 
+    // outputs
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
+
+    // conda functions
+    conda.test();
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
