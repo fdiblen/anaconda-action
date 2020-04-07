3d760dc69ec9318b6df3d09d3b9705233e8d9c9c
Faruk D <f.diblen@esciencecenter.nl>
2020-04-07 19:59:23 +0200

    fix text command

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ shell.set('-e');
 module.exports = {
 
     file_exists : function (path) {
-        if (test('-f', path)) {
+        if (shell.test('-f', path)) {
             console.log('Found file: ', path)
         } else {
             shell.echo('File does not exist: ', path);
